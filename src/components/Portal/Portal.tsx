140c32362d4051af2b59b09e508da605e92588de
FarzadVav <farzad.vav.work@gmail.com>
2025-07-12 15:31:35 +0330

    fix Portal hydration error

--- src/components/Portal/Portal.tsx
+++ src/components/Portal/Portal.tsx
@@ -3,22 +3,16 @@
 import { createPortal } from "react-dom";
 import { PropsWithChildren } from "react";
 
-import ClientOnly from "../ClientOnly/ClientOnly";
-
 type PortalPropsT = PropsWithChildren & {
   container?: Element;
 };
 
 function Portal({ children, container }: PortalPropsT) {
-  return (
-    <ClientOnly>
-      {createPortal(
-        <>
-          {children}
-        </>,
-        container || document.body
-      )}
-    </ClientOnly>
+  return createPortal(
+    <>
+      {children}
+    </>,
+    container || document.body
   );
 };
 
