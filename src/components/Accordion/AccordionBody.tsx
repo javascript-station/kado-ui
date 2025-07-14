44e4f3889b3e5b5869e9eb2abdc72b89b8d2f7e1
FarzadVav <farzad.vav.work@gmail.com>
2025-07-14 20:02:24 +0330

    fix AccordionBody child space

--- src/components/Accordion/AccordionBody.tsx
+++ src/components/Accordion/AccordionBody.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { use } from "react";
+import { ReactNode, use } from "react";
 import { AnimatePresence, HTMLMotionProps, motion } from "framer-motion";
 
 import { cn } from "../../utils/cn";
@@ -8,19 +8,23 @@ import AccordionContext from "./AccordionContext";
 
 type AccordionBodyPropsT = HTMLMotionProps<"div">;
 
-function AccordionBody({ className, ...props }: AccordionBodyPropsT) {
+function AccordionBody({ children, className, ...props }: AccordionBodyPropsT) {
   const { isOpen } = use(AccordionContext);
 
   return (
     <AnimatePresence>
       {isOpen ?
         <motion.div
-          className={cn("overflow-hidden [&_>_*]:m-3", className)}
+          className={cn("overflow-hidden", className)}
           initial={{ height: 0, opacity: 0 }}
           animate={{ height: "auto", opacity: 1 }}
           exit={{ height: 0, opacity: 0 }}
           {...props}
-        />
+        >
+          <div className="m-3">
+            {children as ReactNode}
+          </div>
+        </motion.div>
         : null}
     </AnimatePresence>
   )
